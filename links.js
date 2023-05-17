67ecc0207928866b1e93c60bc96de7acc308a3f3
apd-dsm <nexap.in@gmail.com>
2023-05-17 16:09:51 +0530

    monthly

--- links.js
+++ links.js
@@ -1,25 +1,74 @@
 const monthlyReportLinks = [
     {
-        lbl: "Report 1",
-        link: "https://google.com"
+        lbl: "Viral Hepatitis Screening of Pregnant Women ",
+        link: "https://docs.google.com/spreadsheets/d/1PLyVQCZ2sVxlhZ5aLkUxYitWII8741LuysglSx0RnbI/edit#gid=716815382"
     },
     {
-        lbl: "Report 1",
-        link: "https://google.com"
+        lbl: "RCH & CAMP",
+        link: "https://docs.google.com/spreadsheets/d/1OSCZS6f5H1Oq0JxD5OfkHQBbwKKYz4VHKfFhHdBc4R0/edit#gid=0"
     },
     {
-        lbl: "Report 1",
-        link: "https://google.com"
+        lbl: "Monthly VHND Report",
+        link: "https://docs.google.com/spreadsheets/d/1W3XbWHwcJQBmDLQrzdkLPb_kbJm2Rr1u/edit#gid=490395539"
+    },
+    
+    {
+        lbl: "Meeting with newly married couples for Family Planning",
+        link: "https://docs.google.com/spreadsheets/d/1Rfz6bgaay5AP8sTuDGNToKoxac17_Nf0uh59CkXshfE/edit#gid=0"
     },
     {
-        lbl: "Report 1",
-        link: "https://google.com"
+        lbl: "Convergence of RCH activities between H&FW Dept. and WCD Dept Monthly Summary Report",
+        link: "https://docs.google.com/spreadsheets/d/1_3luXGYIA2Mz-b35rpgKDoyRGxROdV8xfla9WrXWW-k/edit#gid=1475702122"
+    },
+    {
+        lbl: "ANC Clinic Report - Alipurduar",
+        link: "https://docs.google.com/spreadsheets/d/1jVKSFmcGH6oNw3Sy6VEspC3sMn8HCQI4Z7PyOAVF8cs/edit#gid=75953919"
+    },
+    {
+        lbl: " SNSU reporting Sheet",
+        link: "https://docs.google.com/spreadsheets/d/1friorifgRI6SDe8778ocBmHf_CDPClPfM89sGitPLRY/edit#gid=0"
+    },
+    {
+        lbl: "SNCU Follow up record (Facility and Community) Monthly Report Fin year",
+        link: "https://docs.google.com/spreadsheets/d/1iBYtFtqaFe8ll-e9rG5RRPh2Cz4EGVe5k2ag0xIDyfg/edit#gid=1624739859"
+    },
+    {
+        lbl: "NSV REPORT",
+        link: "https://docs.google.com/spreadsheets/d/14VChraU44qrUO5og0xl8eUYd1fOptI1L9k0hKUghf1M/edit#gid=0"
+    },
+    {
+        lbl: "PMSMA (monthly)",
+        link: "https://docs.google.com/spreadsheets/d/1FEi-6VsVcIocF1t0SCXjlgu96dTEf-JW3QK0ADJFCXk/edit#gid=0"
+    },
+    {
+        lbl: "Monthly Partograph Report 2023-24 - COMPILED",
+        link: "https://docs.google.com/spreadsheets/d/1yFoyZOC-C9wtZSlBkgzs6LCmPnNtJODt/edit#gid=744680099"
+    },
+    {
+        lbl: "Monthly Antenatal Corticosteroid Report 2023-24 - COMPILED",
+        link: "https://docs.google.com/spreadsheets/d/1tmyPiV2tCNAzJbl6lIOSNmZHomv4VWAp/edit#gid=1471033130"
+    },
+    {
+        lbl: "2023-24_CEmOC & LSAS Performance Tracking Monitoring Sheet",
+        link: "https://docs.google.com/spreadsheets/d/1dO9ZdRNsPQ8vnGBNENJWHEK-Xw5xfuJnffowDfgHGKs/edit#gid=113799590"
     },
 ];
 
 
 const quarterlyReportLinks = [
     {
+        lbl: "Free Contraceptive Quarterly",
+        link: "https://docs.google.com/spreadsheets/d/1GOUbSHgt-LCweAJeKmyjwQYg9VGzrUgeY1eDbDuXsPg/edit#gid=0"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
         lbl: "Report 1",
         link: "https://google.com"
     },
@@ -35,6 +84,15 @@ const quarterlyReportLinks = [
         lbl: "Report 1",
         link: "https://google.com"
     },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    
 ];
 
 
