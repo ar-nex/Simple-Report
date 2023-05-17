1d5ed54a43fc8f37d93493f661a018a1f67d5a99
apd-dsm <nexap.in@gmail.com>
2023-05-17 12:55:34 +0530

    structure commit

---
+++ links.js
@@ -0,0 +1,58 @@
+const monthlyReportLinks = [
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+];
+
+
+const quarterlyReportLinks = [
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+];
+
+
+const programReportLinks = [
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+    {
+        lbl: "Report 1",
+        link: "https://google.com"
+    },
+];
