b86c5a63038edfa1e75c0a47e46e4ab5150e3738
apd-dsm <nexap.in@gmail.com>
2023-06-12 15:50:06 +0530

    capadded

--- links.js
+++ links.js
@@ -1,64 +1,79 @@
 const monthlyReportLinks = [
     {
         lbl: "Viral Hepatitis Screening of Pregnant Women ",
-        link: "https://docs.google.com/spreadsheets/d/1PLyVQCZ2sVxlhZ5aLkUxYitWII8741LuysglSx0RnbI/edit#gid=716815382"
+        link: "https://docs.google.com/spreadsheets/d/1PLyVQCZ2sVxlhZ5aLkUxYitWII8741LuysglSx0RnbI/edit#gid=716815382",
+        cap: "Hospital | block"
     },
     {
         lbl: "RCH & CAMP",
-        link: "https://docs.google.com/spreadsheets/d/1OSCZS6f5H1Oq0JxD5OfkHQBbwKKYz4VHKfFhHdBc4R0/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1OSCZS6f5H1Oq0JxD5OfkHQBbwKKYz4VHKfFhHdBc4R0/edit#gid=0",
+        cap: ""
     },
     {
         lbl: "Monthly VHND Report",
-        link: "https://docs.google.com/spreadsheets/d/1W3XbWHwcJQBmDLQrzdkLPb_kbJm2Rr1u/edit#gid=490395539"
+        link: "https://docs.google.com/spreadsheets/d/1W3XbWHwcJQBmDLQrzdkLPb_kbJm2Rr1u/edit#gid=490395539",
+        cap: "Only block"
     },
     
     {
         lbl: "Meeting with newly married couples for Family Planning",
-        link: "https://docs.google.com/spreadsheets/d/1Rfz6bgaay5AP8sTuDGNToKoxac17_Nf0uh59CkXshfE/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1Rfz6bgaay5AP8sTuDGNToKoxac17_Nf0uh59CkXshfE/edit#gid=0",
+        cap: "Only block"
     },
     {
         lbl: "Convergence of RCH activities between H&FW Dept. and WCD Dept Monthly Summary Report",
-        link: "https://docs.google.com/spreadsheets/d/1_3luXGYIA2Mz-b35rpgKDoyRGxROdV8xfla9WrXWW-k/edit#gid=1475702122"
+        link: "https://docs.google.com/spreadsheets/d/1_3luXGYIA2Mz-b35rpgKDoyRGxROdV8xfla9WrXWW-k/edit#gid=1475702122",
+        cap: "Only block"
     },
     {
         lbl: "ANC Clinic Report - Alipurduar",
-        link: "https://docs.google.com/spreadsheets/d/1jVKSFmcGH6oNw3Sy6VEspC3sMn8HCQI4Z7PyOAVF8cs/edit#gid=75953919"
+        link: "https://docs.google.com/spreadsheets/d/1jVKSFmcGH6oNw3Sy6VEspC3sMn8HCQI4Z7PyOAVF8cs/edit#gid=75953919",
+        cap: ""
     },
     {
         lbl: " SNSU reporting Sheet",
-        link: "https://docs.google.com/spreadsheets/d/1friorifgRI6SDe8778ocBmHf_CDPClPfM89sGitPLRY/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1friorifgRI6SDe8778ocBmHf_CDPClPfM89sGitPLRY/edit#gid=0",
+        cap: ""
     },
     {
         lbl: "SNCU Follow up record (Facility and Community) Monthly Report Fin year",
-        link: "https://docs.google.com/spreadsheets/d/1iBYtFtqaFe8ll-e9rG5RRPh2Cz4EGVe5k2ag0xIDyfg/edit#gid=1624739859"
+        link: "https://docs.google.com/spreadsheets/d/1iBYtFtqaFe8ll-e9rG5RRPh2Cz4EGVe5k2ag0xIDyfg/edit#gid=1624739859",
+        cap: "",
     },
     {
         lbl: "NSV REPORT",
-        link: "https://docs.google.com/spreadsheets/d/14VChraU44qrUO5og0xl8eUYd1fOptI1L9k0hKUghf1M/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/14VChraU44qrUO5og0xl8eUYd1fOptI1L9k0hKUghf1M/edit#gid=0",
+        cap: ""
     },
     {
         lbl: "PMSMA (monthly)",
-        link: "https://docs.google.com/spreadsheets/d/1FEi-6VsVcIocF1t0SCXjlgu96dTEf-JW3QK0ADJFCXk/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1FEi-6VsVcIocF1t0SCXjlgu96dTEf-JW3QK0ADJFCXk/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "Monthly Partograph Report 2023-24 - COMPILED",
-        link: "https://docs.google.com/spreadsheets/d/1yFoyZOC-C9wtZSlBkgzs6LCmPnNtJODt/edit#gid=744680099"
+        link: "https://docs.google.com/spreadsheets/d/1yFoyZOC-C9wtZSlBkgzs6LCmPnNtJODt/edit#gid=744680099",
+        cap: ""
     },
     {
         lbl: "Monthly Antenatal Corticosteroid Report 2023-24 - COMPILED",
-        link: "https://docs.google.com/spreadsheets/d/1tmyPiV2tCNAzJbl6lIOSNmZHomv4VWAp/edit#gid=1471033130"
+        link: "https://docs.google.com/spreadsheets/d/1tmyPiV2tCNAzJbl6lIOSNmZHomv4VWAp/edit#gid=1471033130",
+        cap: ""
     },
     {
         lbl: "2023-24_CEmOC & LSAS Performance Tracking Monitoring Sheet",
-        link: "https://docs.google.com/spreadsheets/d/1dO9ZdRNsPQ8vnGBNENJWHEK-Xw5xfuJnffowDfgHGKs/edit#gid=113799590"
+        link: "https://docs.google.com/spreadsheets/d/1dO9ZdRNsPQ8vnGBNENJWHEK-Xw5xfuJnffowDfgHGKs/edit#gid=113799590",
+        cap: "",
     },
     {
         lbl: " DP - KPI Report",
-        link: "https://docs.google.com/spreadsheets/d/1WDdL4ehEV_kDvl4_yePR9-mnD6jtvpbz7nD3xVUKR7w/edit#gid=766286292"
+        link: "https://docs.google.com/spreadsheets/d/1WDdL4ehEV_kDvl4_yePR9-mnD6jtvpbz7nD3xVUKR7w/edit#gid=766286292",
+        cap: "",
     },
     {
         lbl: " CAC Report ",
-        link: "https://docs.google.com/spreadsheets/d/1vgaw0eAsovlvhz8QtevP8uErUZMhVfFi5mpGbq8VhYo/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1vgaw0eAsovlvhz8QtevP8uErUZMhVfFi5mpGbq8VhYo/edit#gid=0",
+        cap: "",
     },
 ];
 
@@ -66,27 +81,33 @@ const monthlyReportLinks = [
 const quarterlyReportLinks = [
     {
         lbl: "Free Contraceptive Quarterly",
-        link: "https://docs.google.com/spreadsheets/d/1GOUbSHgt-LCweAJeKmyjwQYg9VGzrUgeY1eDbDuXsPg/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1GOUbSHgt-LCweAJeKmyjwQYg9VGzrUgeY1eDbDuXsPg/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "PTK Report",
-        link: "https://docs.google.com/spreadsheets/d/1_uDC3DMMKNqscqtRT4rZJjwaPGjhe9-CdLKw_uERI54/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1_uDC3DMMKNqscqtRT4rZJjwaPGjhe9-CdLKw_uERI54/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "LMIS",
-        link: "https://docs.google.com/spreadsheets/d/1LANeOXmUehch8SSNQmh4BLjKTOF-7N5hAozh3BJGrww/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1LANeOXmUehch8SSNQmh4BLjKTOF-7N5hAozh3BJGrww/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "FPIS-IA-Entry",
-        link: "https://docs.google.com/spreadsheets/d/18L1N1f4x5iwhgjriQ6QfSVtzErOs_YLv2ttxo2hCxuY/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/18L1N1f4x5iwhgjriQ6QfSVtzErOs_YLv2ttxo2hCxuY/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "ESB Entry Screen",
-        link: "https://docs.google.com/spreadsheets/d/1BeiT-qzUqJKJSzc7bBf2mDEUnnwHUtmISKzMb6jTmgE/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1BeiT-qzUqJKJSzc7bBf2mDEUnnwHUtmISKzMb6jTmgE/edit#gid=0",
+        cap: "",
     },
     {
         lbl: "Training Status Entry",
-        link: "https://docs.google.com/spreadsheets/d/1-CVi2ONQiwefJamwuyECffm2A5f2tWQIRNbIlSLeJ9g/edit#gid=0"
+        link: "https://docs.google.com/spreadsheets/d/1-CVi2ONQiwefJamwuyECffm2A5f2tWQIRNbIlSLeJ9g/edit#gid=0",
+        cap: ""
     }
    
     
